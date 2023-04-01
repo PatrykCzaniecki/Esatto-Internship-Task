df01ea25590795e5004a09dc1b28e42a4d8ce1cb
PatrykCzaniecki <93118979+PatrykCzaniecki@users.noreply.github.com>
2023-04-01 13:49:35 +0200

    Add customer class model

--- EsattoApp/Customer.cs
+++ EsattoApp/Customer.cs
@@ -1,6 +1,10 @@
-﻿namespace EsattoApp
+﻿namespace EsattoApp;
+
+public class Customer
 {
-    public class Customer
-    {
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string VatIdentificationNumber { get; set; }
+    public DateTime CreationDate { get; set; }
+    public string Address { get; set; }
 }
